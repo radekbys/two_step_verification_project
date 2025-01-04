416c7326b7977af5813fbf18b7a07f19cbfbd35d
radekbys <radekbry12@gmail.com>
2025-01-04 14:34:02 +0100

    added dist/ to gitignore

--- frontend/src/components/LoginForm/LoginForm.tsx
+++ frontend/src/components/LoginForm/LoginForm.tsx
@@ -1,12 +1,48 @@
 import './LoginForm.css'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function LoginForm () {
   const navigate = useNavigate()
+  const [loggingFailed, setLoggingFailed] = useState(false)
+  const [inputEmail, setInputEmail] = useState('')
+  const [inputPassword, setInputPassword] = useState('')
 
-  const submitLoginForm = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const submitLoginForm = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
     event.preventDefault()
+
+    const data = {
+      email: inputEmail,
+      password: inputPassword
+    }
+    const jsonData = JSON.stringify(data)
+    const headers = new Headers()
+    headers.append('Content-Type', 'application/json')
+
+    const res = await fetch('http://localhost:4000/login/firstStep', {
+      method: 'POST',
+      headers: headers,
+      body: jsonData
+    })
+    if (!res.ok) {
+      setLoggingFailed(true)
+      return
+    }
+
+    localStorage.setItem('Email', inputEmail)
+    localStorage.setItem('Password', inputPassword)
+
     navigate('/checkAuthorization')
+  }
+
+  if (loggingFailed) {
+    return (
+      <>
+        <h1>Logging failed, refresh and retry</h1>
+      </>
+    )
   }
 
   return (
@@ -15,11 +51,21 @@ export default function LoginForm () {
         <h1>Please log in</h1>
         <div className='FormField'>
           <label>Email:</label>
-          <input type='text' />
+          <input
+            className='userEmailInput'
+            value={inputEmail}
+            onChange={e => setInputEmail(e.target.value)}
+            type='email'
+          />
         </div>
         <div className='FormField'>
           <label>Password:</label>
-          <input type='password' />
+          <input
+            className='userPasswordInput'
+            value={inputPassword}
+            onChange={e => setInputPassword(e.target.value)}
+            type='password'
+          />
         </div>
         <button type='submit' onClick={submitLoginForm}>
           Submit
