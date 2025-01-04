60042bf05de51be8cf9f055675fb0ad6411b1bf0
radekbys <radekbry12@gmail.com>
2025-01-04 15:29:15 +0100

    finished

--- frontend/src/components/AuthorizationCodeForm/AuthorizationCodeForm.tsx
+++ frontend/src/components/AuthorizationCodeForm/AuthorizationCodeForm.tsx
@@ -1,14 +1,50 @@
 import { useNavigate } from 'react-router-dom'
 import './AuthorizationCodeForm.css'
+import { useState } from 'react'
 
 export default function AuthorizationCodeForm () {
   const navigate = useNavigate()
+  const [verCode, setVerCode] = useState('')
+  const [loggingFailed, setLoggingFailed] = useState(false)
 
-  const submitAuthorizationCodeForm = (
+  const submitAuthorizationCodeForm = async (
     event: React.MouseEvent<HTMLButtonElement>
   ) => {
     event.preventDefault()
+
+    const reqObj = {
+      email: localStorage.getItem('Email'),
+      password: localStorage.getItem('Password'),
+      code: verCode
+    }
+
+    const reqBodyJson = JSON.stringify(reqObj)
+    const headers = new Headers()
+    headers.append('Content-Type', 'application/json')
+
+    const res = await fetch('http://localhost:4000/login/secondStep', {
+      method: 'POST',
+      headers: headers,
+      body: reqBodyJson
+    })
+    if (!res.ok) {
+      setLoggingFailed(true)
+      return
+    }
+
+    const resObj = await res.json()
+
+    console.log(resObj)
+
     navigate('/loggedInPage')
+  }
+
+  if (loggingFailed) {
+    return (
+      <>
+        <h1>Authorization failed, refresh and retry</h1>
+      </>
+    )
   }
 
   return (
@@ -20,10 +56,15 @@ export default function AuthorizationCodeForm () {
         </p>
         <div className='FormField'>
           <label>Authorization Code:</label>
-          <input type='text' />
+          <input
+            type='text'
+            value={verCode}
+            onChange={e => {
+              setVerCode(e.target.value)
+            }}
+          />
         </div>
         <button type='submit' onClick={submitAuthorizationCodeForm}>
-          {' '}
           Submit
         </button>
       </form>
